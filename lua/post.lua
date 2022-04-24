2bdf14d77f29ace81d337549bb2f5441db1c21a6
Kennytian <kt.tian@gmail.com>
2022-04-24 14:45:00 +0800

    feat: Update post.lua file

--- lua/post.lua
+++ lua/post.lua
@@ -1,4 +1,4 @@
---curl -d {"name": "Jack"}  http://localhost:8084/post
+-- curl -d '{"name": "Jack"}'  http://localhost:8084/post
 ngx.header['server'] = 'secret';
 ngx.req.read_body();
 local data = ngx.req.get_body_data();
